b1cb33e184b6a48e3394e68426a69c17dd236aac
Jinish Shrestha <dixon.cider014@gmail.com>
2024-10-14 19:33:25 +0545

    project completed

--- backend/server.js
+++ backend/server.js
@@ -15,16 +15,16 @@ const __dirname = path.resolve()
 app.use(express.json())//ALLOWS USE TO ACCEPT JSON DATA IN req.body
 const port = process.env.PORT || 5000
 
-app.get("/", (req, res) => { //to create a route
-    res.send("Server is ready")
-})
+// app.get("/", (req, res) => { //to create a route
+//     res.send("Server is ready")
+// })
 
 app.use("/api/products", productRoutes)
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "frontend/dist")))
     app.get("*", (req, res) => {
-
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
     })
 }
 app.listen(port, () => {
